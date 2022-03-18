75e74964b4d061159c52a6bb19f7b95d1685348c
Felix Kressmann <felix@felixkr.de>
2022-03-18 23:53:36 +0100

    First working version

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 #include <PubSubClient.h>
 #include <credentials.h>
 
+#define BUTTON_POWER D6
+#define BUTTON_SET D7
+#define BUTTON_UP D2
+#define BUTTON_DOWN D3
+#define LED D4
+
 const String commandPrefix = MQTT_PREFIX + String("cmnd");
 const char *deviceName = "esp-mobiremote";
 int tempSet;
@@ -14,13 +20,6 @@ ESP8266WiFiMulti wifiMulti;
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
-void writeSettingsToEeprom() {
-  EEPROM.begin(sizeof(tempSet));
-  EEPROM.put(0, tempSet);
-  EEPROM.commit();
-  EEPROM.end();
-}
-
 void sendData(String subtopic, String data, bool retained) {
   Serial.println("SENDING: " + subtopic + ":" + data);
   subtopic = MQTT_PREFIX + subtopic;
@@ -28,9 +27,17 @@ void sendData(String subtopic, String data, bool retained) {
 }
 
 void log(String line) {
-  Serial.println(line);
   sendData("log", line, true);
 }
+
+void writeSettingsToEeprom() {
+  EEPROM.begin(sizeof(tempSet));
+  EEPROM.put(0, tempSet);
+  EEPROM.commit();
+  EEPROM.end();
+  log(String(tempSet) + " written to flash.");
+}
+
 
 void reconnectMqtt() {
   if (!mqttClient.connected()) {
@@ -40,7 +47,7 @@ void reconnectMqtt() {
     // please change following line to    if (client.connect(clientId,userName,passWord))
     if (mqttClient.connect(deviceName, MQTT_USER, MQTT_PASSWORD)) {
       Serial.println("connected");
-      mqttClient.subscribe(commandPrefix.c_str());
+      mqttClient.subscribe((commandPrefix + "/#").c_str());
       sendData("ip", WiFi.localIP().toString(), true);
       sendData("rssi", String(WiFi.RSSI()), true);
     } else {
@@ -52,15 +59,69 @@ void reconnectMqtt() {
   }
 }
 
+void pressButton(int button, boolean longPress) {
+  log("Pressing Button " + String(button));
+  digitalWrite(button, HIGH);
+  delay(200);
+  if (longPress) delay(3000);
+  digitalWrite(button, LOW);
+  delay(100);
+}
+
+void pressButton(int button) {
+  pressButton(button, false);
+}
+
+void changeSetTemperature(int delta) {
+  if (delta == 0) {
+    return;
+  }
+  pressButton(BUTTON_SET);
+  if (delta < 0) {
+    for (int i = 0; i > delta; i--) {
+      pressButton(BUTTON_DOWN);
+    }
+  } else {
+    for (int i = 0; i < delta; i++) {
+      pressButton(BUTTON_UP);
+    }
+  }
+  // wait for mobicool to exit temperature menu before doing anything else
+  delay(11000);
+}
+
+void handleNewSetTemp(int newSetTemp) {
+  if (newSetTemp == tempSet || newSetTemp < -10 || newSetTemp > 20) {
+    log("Bad newTemp: " + String(newSetTemp) + " (oldTemp: " + String(tempSet) + ")");
+    return;
+  }
+  int delta = newSetTemp - tempSet;
+  changeSetTemperature(delta);
+  tempSet = newSetTemp;
+  writeSettingsToEeprom();
+}
+
 void callback(char *topic, byte *payload, unsigned int length) {
-  char pl[length];
-  for (unsigned int i = 0; i < length; i++){
+  digitalWrite(LED, LOW);
+  char pl[length + 1];
+  for (unsigned int i = 0; i < length; i++) {
     pl[i] = payload[i];
   }
-  tempSet = atoi(pl);
-  writeSettingsToEeprom();
-  log(String(tempSet) + " written to flash.");
-  // Handle MQTT
+  pl[length] = '\0';
+
+  char *slashPointer = strrchr(topic, '/');
+
+  if (strcmp(slashPointer + 1, "set") == 0) {
+    handleNewSetTemp(atoi(pl));
+  } else if (strcmp(slashPointer + 1, "init") == 0) {
+    tempSet = atoi(pl);
+    writeSettingsToEeprom();
+  } else if (strcmp(slashPointer + 1, "pwr") == 0) {
+    pressButton(BUTTON_POWER, true);
+  } else {
+    log("Invalid command: " + String(slashPointer) + "=" + String(pl));
+  }
+  digitalWrite(LED, HIGH);
 }
 
 void setup() {
@@ -74,6 +135,12 @@ void setup() {
     delay(1000);
     Serial.println("WiFi running...");
   }
+  ArduinoOTA.onStart([]() {
+    log("Start OTA update");
+  });
+
+  ArduinoOTA.setHostname(deviceName);
+  ArduinoOTA.begin();
 
   // Read settings from EEPROM
   EEPROM.begin(sizeof(tempSet));
@@ -82,11 +149,16 @@ void setup() {
   Serial.println("TempSet from EEPROM: " + String(tempSet));
   //
 
+  pinMode(BUTTON_POWER, OUTPUT);
+  pinMode(BUTTON_SET, OUTPUT);
+  pinMode(BUTTON_UP, OUTPUT);
+  pinMode(BUTTON_DOWN, OUTPUT);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+
   mqttClient.setServer(MQTT_SERVER, 1883);
   mqttClient.setCallback(callback);
 
-  ArduinoOTA.setHostname(deviceName);
-  ArduinoOTA.begin();
   Serial.println("Setup finished, looping now");
 }
 
